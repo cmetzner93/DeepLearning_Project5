cfd7b82f34f2e2a5ec4ed643b79f38e740c7bc99
cmetzner93 <cmetzner@vols.utk.edu>
2020-04-28 10:29:27 -0400

    update code

--- pycharm_code/cDCGAN_utils.py
+++ pycharm_code/cDCGAN_utils.py
@@ -10,11 +10,12 @@ def generate_and_save_images(model, epoch, test_input, test_labels):
     fig = plt.figure(figsize=(4, 5))
     for i in range(predictions.shape[0]):
         plt.subplot(4, 5, i + 1)
-        plt.imshow(predictions[i, :, :, 0] * 255, cmap='gray')
+        plt.imshow((predictions[i, :, :, 0] + 127.5) * 127.5, cmap='gray')
         plt.axis('off')
 
     plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
     #plt.show()
+    plt.close('all')
 
 
 # Function store metrics in text file
